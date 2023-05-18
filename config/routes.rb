5c283c6ca4e92151274b11c71d1f9c75dc3b686a
James England <jimmy.england@kellogg.northwestern.edu>
2023-05-18 02:13:00 +0000

    Working routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/create'
+  get 'places/index'
+  get 'places/show'
+  get 'places/new'
+  get 'places/create'
+
+  root to: 'places#index'
+
+  resources :places, only: [:index, :show, :new, :create] do
+    resources :posts, only: [:new, :create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
+
+
 end
